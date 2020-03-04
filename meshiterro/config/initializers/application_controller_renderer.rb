eb4076e3a14dc06155c8c40d392c5b80f691c628
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-03-04 19:47:32 +0900

    [Finish]5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '84cc61509be816ece29e50405943a340e81ceb119f5725f9fe8abfad351840b389977a83b78810e5605eaaae0ebfa4216ca183cfb88812fe955cd7a68dac4a18'
